0aa2004aad63c6af5d1e134c5c6b25fc019a1351
Nelson Pulitano <nelpno@gmail.com>
2026-03-02 15:55:39 +0000

    File changes

--- src/components/ui/sidebar.jsx
+++ src/components/ui/sidebar.jsx
@@ -462,7 +462,7 @@ const SidebarMenuButton = React.forwardRef(
         data-size={size}
         data-active={isActive}
         className={cn(sidebarMenuButtonVariants({ variant, size }), className)}
-        {...props} />
+        {...rest} />
     );
 
     if (!tooltip) {
