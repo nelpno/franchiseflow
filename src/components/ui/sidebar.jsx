36f049d375d23f26c45d9a8af3fec2878cf24ff4
Nelson Pulitano <nelpno@gmail.com>
2026-03-02 14:43:00 +0000

    File changes

--- src/components/ui/sidebar.jsx
+++ src/components/ui/sidebar.jsx
@@ -488,7 +488,6 @@ const SidebarMenuButton = React.forwardRef(({
 })
 SidebarMenuButton.displayName = "SidebarMenuButton"
 
-
 const SidebarMenuAction = React.forwardRef(({ className, asChild = false, showOnHover = false, ...props }, ref) => {
   const Comp = asChild ? Slot : "button"
 
